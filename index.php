e53d76fe7841b7d2bc7792f768db76265ca9aaae
Dheerajsingh002 <dheerajsingh8357@gmail.com>
2023-01-05 21:04:17 +0530

    socialapi

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Social Network Apis</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Quicksand:wght@700&display=swap" rel="stylesheet">
+    <style>
+        body{
+            background:url('https://tophinhanhdep.com/wp-content/uploads/2021/11/Social-Media-Wallpapers.jpg') center center fixed;
+            background-size:cover;
+            text-align:center;
+            font-family:Arvo,serif;
+            color:white;
+        }
+        .jumbotron{
+            background:transparent;
+        }
+        .jumbotron h1{
+            letter-spacing:2.5px;
+        }
+        .form-horizontal{
+            margin-top:50px;
+        }
+    </style>
+    
+  </head>
+  <body>
+  <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v15.0" nonce="U1czBe3k"></script>
+    <div class="jumbotron">
+        <div class="container-fluid">
+        <h1>Join us and build Freedom</h1>
+        <p>We make freedom ,we love freedom, we spread freedom</p>
+        </div>
+        <form action="" class="form-horizontal">
+        <div class="form-group">
+            <div class="col-xs-offset-2 col-xs-8">
+                <input type="email" id="email" placeholder="Your Email" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+                <div class="col-xs-offset-3 col-xs-6">
+                    <input type="submit" id="submit" value="Subscribe" class="btn btn-info btn-lg">
+                </div>
+            </div>
+    </form>
+    <div class="row">
+        <div class="col-sm-offset-3 col-sm-2">
+        <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
+        </div>
+        <div class="col-sm-2">
+        
+         <g:plusone size="tall"></g:plusone>
+
+        </div>
+       <div class="col-sm-2">
+
+       </div>
+    </div>
+    <div>
+
+    </div>
+        
+    </div>
+   
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </body>
+</html>
